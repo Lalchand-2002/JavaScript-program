61caad8651f1d78026429e246f9e727ea889a1b8
Lalchand-2002 <mrhansda12@gmail.com>
2024-07-26 22:59:26 +0530

    student result grade

---
+++ grade.js
@@ -0,0 +1,21 @@
+
+ 
+let scores=76;
+if(scores>=90){
+    console.log("Grade:- A");
+}
+else if(scores >=80&& scores<=89){
+    console.log("Grade:- B");
+}
+else if(scores>=70&& scores<=79){
+    console.log("Grade:- C");
+}
+else if(scores>=60&& scores<=69){
+    console.log("Grade:- D");
+}
+else if(scores<=60){
+    console.log("Grade:- F");
+}
+else if(scores>100){
+    console.log("Invalid Scorec");
+}
